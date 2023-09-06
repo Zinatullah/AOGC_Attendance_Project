9395ef2dd2708ecae5be794e14151a193f117c6a
Zinatullah AKRAMI <zinatullahakrami@gmail.com>
2023-09-06 10:23:37 +0430

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
 const express = require('express')
 const Color = require('color');
 const bodyParser = require('body-parser')
@@ -23,6 +26,18 @@ app.use('/api/devices', require("./routes/devicesRoutes"))
 app.use('/api/employees', require("./routes/employeesRoutes"))
 app.use('/api/logs', require("./routes/logsRoutes"))
 // app.use('/api/goals', require("./routes/goalRoutes"))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.use(bodyParser.urlencoded({extended: true}))
 
